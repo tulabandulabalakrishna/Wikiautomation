473ca7bb7ce9a3a4e6dd28a713556ada9119504f
Pradeep Chelimala <Pradeep_Chelimala@epam.com>
2018-08-02 20:22:44 +0530

    adding files

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,35 @@
+
+
+import com.epam.core.GuiComponents;
+import org.junit.runner.*;
+import org.junit.runner.notification.Failure;
+import testsecnarios.WikiSearch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class TestRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestRunner.class);
+    public static void main(String args[])
+    {
+
+        try
+        {
+            logger.info("Test execution is started" );
+            Result result = JUnitCore.runClasses(WikiSearch.class);
+            for(Failure failure : result.getFailures())
+            {
+                failure.getMessage();
+            }
+            logger.info("Test Execution is ended" );
+        }
+        catch(Exception e)
+        {
+            logger.info(e.getMessage());
+            logger.info("Test Execution is ended" );
+        }
+
+    }
+
+}
